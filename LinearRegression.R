e3c52dd117bb258e5e907e22dd80aa0b90e6ecc6
Amlan Kumar Pradhan <34065551+amlan123@users.noreply.github.com>
2018-01-25 09:57:50 +0530

    Update LinearRegression.R

--- LinearRegression.R
+++ LinearRegression.R
@@ -3,7 +3,7 @@
 #load directory
 getwd()
 
-> setwd("C:/Users/amlan/Documents/RPRAC")
+> setwd("C:/Users")
 
 #load data
 train <- read.csv("C:/Users/amlan/Documents/RPRAC/Train_UWu5bXk.csv")
@@ -18,7 +18,7 @@ combi <- rbind(train, test)
 #impute missing value in Item_Weight
 combi$Item_Weight[is.na(combi$Item_Weight)] <- median(combi$Item_Weight, na.rm = TRUE)
 
-#impute 0 in item_visibility
+#impute zero in item_visibility
 combi$Item_Visibility <- ifelse(combi$Item_Visibility == 0, median(combi$Item_Visibility),combi$Item_Visibility)
 
 #rename level in Outlet_Size
@@ -29,8 +29,8 @@ library(plyr)
 combi$Item_Fat_Content <- revalue(combi$Item_Fat_Content,c("LF" = "Low Fat", "reg" = "Regular"))
 combi$Item_Fat_Content <- revalue(combi$Item_Fat_Content, c("low fat" = "Low Fat"))
 
-#create a new column 2013 - Year
-combi$Year <- 2013 - combi$Outlet_Establishment_Year
+#create a new column 2012 - Year
+combi$Year <- 2012 - combi$Outlet_Establishment_Year
 
 #drop variables not required in modeling
 library(dplyr)
@@ -49,3 +49,4 @@ linear_model <- lm(log(Item_Outlet_Sales) ~ ., data = new_train)
 summary(linear_model)
 plot(linear_model)
 
+#check the R square value to know how better your model is.
